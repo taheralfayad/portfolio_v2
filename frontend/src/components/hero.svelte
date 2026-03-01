935e248b4df7ec7be4b457767c7923fb2892873d
Taher Alfayad <taheralfayad@gmail.com>
2026-03-01 16:47:57 -0500

    feat(home): make hero section content more dynamic

--- frontend/src/components/hero.svelte
+++ frontend/src/components/hero.svelte
@@ -1,11 +1,13 @@
 <script>
-  import Carousel from '../design-system/carousel.svelte';
+  import Carousel from "../design-system/carousel.svelte";
 
-  let { carouselImages } = $props();
+  let { header, subtitle, carouselImages } = $props();
 </script>
 
 <div class="flex flex-col p-2 mt-12 items-center text-center">
-  <h1 class="text-xl">Hello! My name is Taher Alfayad.</h1>
-  <p>I'm a husband, older brother, dog dad, Arsenal fan, and software developer.</p>
-  <Carousel images={carouselImages}/>
+  <h1 class="text-xl">{header}</h1>
+  <p>
+    {subtitle}
+  </p>
+  <Carousel images={carouselImages} />
 </div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,14 +1,15 @@
 <script>
-  import { goto } from '$app/navigation';
-  import Hero from "../components/hero.svelte"; 
+  import { goto } from "$app/navigation";
+  import Hero from "../components/hero.svelte";
   import WorkProjects from "../components/work_projects.svelte";
   import PersonalProjects from "../components/personal_projects.svelte";
   import ExperienceFlow from "../components/experience_flow.svelte";
   import SkillsTable from "../components/skills_table.svelte";
   import NavBar from "../components/navbar.svelte";
-  import { onMount } from 'svelte';
+  import { onMount } from "svelte";
 
   import { api } from "../utils/api.svelte.js";
+  import Navbar from "../components/navbar.svelte";
 
   let workExperiences = $state([]);
   let workProjects = $state([]);
@@ -16,41 +17,47 @@
   let skills = $state([]);
   let images = $state([]);
 
-  // let currNavValue = $state("Home");
+  const navItems = [
+    {
+      value: "Home",
+      header: "Hello! My name is Taher Alfayad.",
+      subtitle:
+        "I'm a husband, older brother, dog dad, Arsenal fan, and software developer.",
+      onClick: () => {
+        currNavValue = navItems.find((item) => item.value === "Home");
+      },
+    },
+    {
+      value: "Coffee",
+      header: "Hello! Join me on my coffee journey.",
+      subtitle:
+        "I love drinking coffee. I've recently started roasting my own, and this is where I document it.",
+      onClick: () => {
+        currNavValue = navItems.find((item) => item.value === "Coffee");
+      },
+    },
+  ];
 
-  // const navBarItems = [
-  //   {
-  //     "value": "Home",
-  //     "onClick": () => {
-  //       goto('#');
-  //       currNavValue = 'Home';
-  //     }
-  //   },
-  //   {
-  //     "value": "Blog",
-  //     "onClick": () => {
-  //       goto('#')
-  //       currNavValue = 'Blog';
-  //     }
-  //   }
-  // ]
+  let currNavValue = $derived.by(() => {
+    return navItems.find((item) => item.value === "Home");
+  });
 
-  // const getCurrNavValue = () => {
-  //   return currNavValue;
-  // }
+  const getCurrNavValue = () => {
+    return currNavValue.value;
+  };
 
   const getWorkExperiences = async () => {
-    const data = await api.get("/work-experiences?limit=3")
+    const data = await api.get("/work-experiences?limit=3");
 
     workExperiences = data.map((datum) => ({
       title: datum.title,
       subtitle: datum.workplace,
       description: datum.description,
-    }))
-  }
+    }));
+  };
 
   const getWorkProjects = async () => {
-    const data = await api.get("/projects?limit=5&type=work")
+    const data = await api.get("/projects?limit=5&type=work");
 
     workProjects = data.map((datum) => ({
       name: datum.name,
@@ -59,11 +66,11 @@
       blogLink: datum.blog_link,
       image: datum.image,
       type: datum.type,
-    }))
-  }
+    }));
+  };
 
   const getPersonalProjects = async () => {
-    const data = await api.get("/projects?limit=5&type=personal")
+    const data = await api.get("/projects?limit=5&type=personal");
 
     personalProjects = data.map((datum) => ({
       name: datum.name,
@@ -72,31 +79,30 @@
       blogLink: datum.blog_link,
       image: datum.image,
       type: datum.type,
-    }))
-  }
+    }));
+  };
 
   const getSkills = async () => {
-    const data = await api.get("/skills")
+    const data = await api.get("/skills");
 
     skills = data.map((datum) => ({
       name: datum.name,
       category: datum.category,
       blogLink: datum.blog_link,
-    }))
-  }
+    }));
+  };
 
   const getImages = async () => {
-    const data = await api.get("/images")
+    const data = await api.get("/images");
 
-    images = data.map(datum => {
+    images = data.map((datum) => {
       return {
         title: datum.title,
         caption: datum.caption,
-        imageLink: datum.image
-      }
-    })
-
-  }
+        imageLink: datum.image,
+      };
+    });
+  };
 
   onMount(() => {
     getWorkExperiences();
@@ -105,20 +111,42 @@
     getSkills();
     getImages();
   });
-  
 </script>
 
-
-<div class="flex flex-col">
-  <section class="flex items-center justify-center">
-    <Hero carouselImages={images}/>
-  </section>
-  <h2 class="flex justify-center text-xl text-center">Here's a little something about my work experience:</h2>
-  <ExperienceFlow items={workExperiences}/>
-  <h2 class="flex justify-center text-xl text-center">Here are some of the projects I've worked on at my job:</h2>
-  <WorkProjects projects={workProjects}/>
-  <h2 class="flex justify-center text-xl mt-4 text-center">Here are some of the projects I've worked on in my free time:</h2>
-  <PersonalProjects projects={personalProjects}/>
-  <h2 class="flex justify-center text-xl mt-4 text-center">Here are some technologies that I've used (and loved enough to talk about using):</h2>
-  <SkillsTable skills={skills}/>
+<Navbar items={navItems} {getCurrNavValue} />
+<div class="flex flex-col min-h-screen pt-20">
+  {#if currNavValue.value === "Home"}
+    <section class="flex items-center justify-center">
+      <Hero
+        carouselImages={images}
+        header={currNavValue.header}
+        subtitle={currNavValue.subtitle}
+      />
+    </section>
+    <h2 class="flex justify-center text-xl text-center">
+      Here's a little something about my work experience:
+    </h2>
+    <ExperienceFlow items={workExperiences} />
+    <h2 class="flex justify-center text-xl text-center">
+      Here are some of the projects I've worked on at my job:
+    </h2>
+    <WorkProjects projects={workProjects} />
+    <h2 class="flex justify-center text-xl mt-4 text-center">
+      Here are some of the projects I've worked on in my free time:
+    </h2>
+    <PersonalProjects projects={personalProjects} />
+    <h2 class="flex justify-center text-xl mt-4 text-center">
+      Here are some technologies that I've used (and loved enough to talk about
+      using):
+    </h2>
+    <SkillsTable {skills} />
+  {:else if currNavValue.value === "Coffee"}
+    <section class="flex items-center justify-center">
+      <Hero
+        carouselImages={images}
+        header={currNavValue.header}
+        subtitle={currNavValue.subtitle}
+      />
+    </section>
+  {/if}
 </div>
