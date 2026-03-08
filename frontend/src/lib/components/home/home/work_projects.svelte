424b2acdc577c877b49d514de3509f5017442915
Taher Alfayad <taheralfayad@gmail.com>
2026-03-08 13:26:02 -0400

    refactor(*projects): fix imports

--- frontend/src/lib/components/home/home/work_projects.svelte
+++ frontend/src/lib/components/home/home/work_projects.svelte
@@ -1,11 +1,11 @@
 <script>
-  import ProjectCard from '../design-system/project_card.svelte';
+  import ProjectCard from "$lib/design-system/project_card.svelte";
 
   let { projects } = $props();
 </script>
 
 <section class="flex flex-wrap justify-center gap-6 py-8 px-24">
   {#each projects as project}
-    <ProjectCard project={project}/>
+    <ProjectCard {project} />
   {/each}
 </section>
