a3ed5a084f606f42fbd5381eaf0d2ee880fc7367
Taher Alfayad <taheralfayad@gmail.com>
2026-07-04 23:26:23 -0400

    feat: redesign hero component

--- frontend/src/lib/components/home/hero.svelte
+++ frontend/src/lib/components/home/hero.svelte
@@ -1,16 +1,14 @@
 <script>
-  import Carousel from "$lib/design-system/carousel.svelte";
-
-  let { header, subtitle, carouselImages, children } = $props();
+	let { header, subtitle, carouselImages, children } = $props();
 </script>
 
-<div class="flex flex-col p-2 items-center text-center">
-  <div class="pb-5">
-    <h1 class="text-xl">{header}</h1>
-    <p>
-      {subtitle}
-    </p>
-  </div>
+<div class="flex flex-row p-2 justify-between gap-20 items-center text-center">
+	<div class="pb-5">
+		<h1 class="text-xl">{header}</h1>
+		<p>
+			{subtitle}
+		</p>
+	</div>
 
-  {@render children()}
+	{@render children()}
 </div>
