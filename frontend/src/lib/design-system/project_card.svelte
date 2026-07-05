cfbbb34ca3f1d104b946f9a67c96a49f41ed4a45
Taher Alfayad <taheralfayad@gmail.com>
2026-07-04 23:24:14 -0400

    refactor: redesign project card

--- frontend/src/lib/design-system/project_card.svelte
+++ frontend/src/lib/design-system/project_card.svelte
@@ -1,48 +1,32 @@
 <script>
-  import { Link } from "@lucide/svelte";
-  import { Newspaper } from "@lucide/svelte";
-
-  let { project } = $props();
-
-  const projectBgClass =
-    {
-      work: "bg-work-project",
-      personal: "bg-personal-project",
-    }[project.type] ?? "bg-gray-200";
+	import { Link } from "@lucide/svelte";
+	import { Newspaper } from "@lucide/svelte";
+	let { project } = $props();
 </script>
 
 <div
-  class={`relative flex flex-col items-center p-8 w-full min-w-[280px] max-w-md
-          border-4 border-black
-          after:content-[''] after:absolute after:inset-0
-          after:bg-black
-          after:translate-x-3 after:translate-y-3
-          after:-z-10
-          ${projectBgClass}`}
+	class="bg-secondary relative flex flex-col items-center p-8 w-full min-w-[280px] max-w-md"
 >
-  <img
-    src={project.image}
-    alt={project.name}
-    class="block max-w-full h-auto border-2 border-black"
-  />
-  <h3 class="text-lg font-semibold mt-4 text-gray-900 leading-tight">
-    {project.name}
-  </h3>
-  <p class="text-center mt-2 text-sm leading-relaxed">
-    {project.description}
-  </p>
-  <div class="flex w-full justify-center gap-4 mt-6">
-    <a
-      href={project.githubLink}
-      class="p-3 bg-gray-100 border-2 border-black hover:cursor-pointer"
-      target="_blank"
-    >
-      <Link />
-    </a>
-    {#if project.blogLink}
-      <a href={project.blogLink} class="p-3 bg-gray-100 border-2 border-black">
-        <Newspaper />
-      </a>
-    {/if}
-  </div>
+	<div class="w-full h-48 flex items-center justify-center">
+		<img
+			src={project.image}
+			alt={project.name}
+			class="max-w-full max-h-full h-auto w-auto object-contain"
+		/>
+	</div>
+	<h3 class="text-xl mt-4 leading-tight">
+		{project.name}
+	</h3>
+	<p class="text-center mt-2 text-sm leading-relaxed">
+		{project.description}
+	</p>
+	<div class="flex w-full justify-center gap-4 mt-auto pt-6">
+		<a
+			href={project.githubLink}
+			class="p-3 hover:cursor-pointer bg-tertiary border"
+			target="_blank"
+		>
+			<Link />
+		</a>
+	</div>
 </div>
