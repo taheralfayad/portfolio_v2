d27d7f6f625375dd6c70759862307b44c88ec7b2
Taher Alfayad <taheralfayad@gmail.com>
2026-07-05 17:15:27 -0400

    refactor(frontend): idiomatic rewrite of nav item

--- frontend/src/lib/components/navbar.svelte
+++ frontend/src/lib/components/navbar.svelte
@@ -1,17 +1,22 @@
 <script>
-	let { items, getCurrNavValue } = $props();
-
-	import NavItem from "$lib/design-system/nav_item.svelte";
+	import { routes } from "$lib/routes/routes.svelte";
 </script>
+
+{#snippet listNavItem(name, fn)}
+	<li class="flex w-full h-full">
+		<a
+			class="flex items-center text-sm cursor-pointer w-full h-full px-12 border-r hover:bg-secondary hover:text-font transition"
+			href={fn()}
+		>
+			{name}
+		</a>
+	</li>
+{/snippet}
 
 <nav class="flex justify-between bg-tertiary text-font border-b h-16">
 	<ul class="flex h-full items-center">
-		{#each items as item}
-			<NavItem
-				value={item.value}
-				onClick={item.onClick}
-				{getCurrNavValue}
-			/>
+		{#each Object.entries(routes) as [name, fn]}
+			{@render listNavItem(name, fn)}
 		{/each}
 	</ul>
 </nav>
