c924542d28338d9053e2855d69d0f6fa6fb5b0ea
Taher Alfayad <taheralfayad@gmail.com>
2026-07-18 14:08:28 -0400

    fix: flex wrap hero

--- frontend/src/lib/components/home/hero.svelte
+++ frontend/src/lib/components/home/hero.svelte
@@ -2,8 +2,10 @@
 	let { header, subtitle, carouselImages, children } = $props();
 </script>
 
-<div class="flex flex-row p-2 justify-between gap-20 items-center text-center">
-	<div class="pb-5">
+<div
+	class="flex flex-wrap sm:flex-row p-2 sm:justify-between md:gap-20 md:mt-6 items-center text-center"
+>
+	<div class="pb-5 max-sm:mt-4">
 		<h1 class="text-xl">{header}</h1>
 		<p>
 			{subtitle}
